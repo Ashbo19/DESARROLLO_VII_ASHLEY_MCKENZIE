29015546577df6e5dc44337bf66fddfda22735dc
Ashbo19 <ashleybonilla507@gmail.com>
2025-08-26 14:19:03 -0500

    Ejercicio práctico: Generador de Patrones completado

---
+++ TALLERES/TALLER_2/generador_patrones.php
@@ -0,0 +1,33 @@
+<?php
+    //bucle for
+    // Bucle for para iterar sobre un array
+    $asterisco = ["*", "**", "***", "****", "*****"];
+      echo "Triangulo rectangulo:<br>";
+      for ($i = 0; $i < count($asterisco); $i++) {
+      echo $asterisco[$i] . "<br>";
+    }
+
+    // bucle while
+    $j = 0;
+    echo "Números impares menores que 20 usando continue:<br>";
+    while ($j < 20) {
+      $j++;
+      if ($j % 2 == 0) {
+        continue;
+      }
+      echo "$j ";
+    }
+    echo "<br><br>";
+
+    //bucle do-while
+    $contador = 10;
+        echo "Contando del 10 al 1 con do-while:<br>";
+        do {
+            if($contador !=5){
+                echo "$contador ";
+            }
+            $contador--;
+        } while ($contador >= 1);
+    echo "<br><br>";
+
+?>
